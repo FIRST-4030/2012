d0757386a80e122e1c35a13b26da75441846c254
Ingyram <Ingyram@10.94.250.27>
2012-02-11 02:41:43 -0800

    Finished Load and ElevatorQueue, added Shoot

--- 2012CommandRobot/src/edu/wpi/first/wpilibj/templates/commands/Load.java
+++ 2012CommandRobot/src/edu/wpi/first/wpilibj/templates/commands/Load.java
@@ -31,6 +31,13 @@ public class Load extends Command {
      * automatically, so we're never ready to stop. If there's a use case to
      * detect the end of the loading sequence this should be updated to trigger
      * on that event
+     * 
+     * Its function is to collect balls from the front of the loader, 
+     * and make room for a new ball if there is a ball at the bottom of the elevator
+     * by moving the balls in the elevator up just enough for a new ball to fit.
+     * The end result of this should be after 3 balls are loaded, they should all be 
+     * fairly close together near the bottom of the elevator, at which point ElevatorQueue
+     * should be called.
      */
     protected boolean isFinished() {
         return false;
