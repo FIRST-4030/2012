2046d2bf49a7c260a55cf202ece8378085b3c4ac
Ingyram <Ingyram@10.94.250.27>
2012-02-11 00:31:11 -0800

    Added some switches

--- 2012CommandRobot/src/edu/wpi/first/wpilibj/templates/subsystems/Switches.java
+++ 2012CommandRobot/src/edu/wpi/first/wpilibj/templates/subsystems/Switches.java
@@ -1,5 +1,6 @@
 package edu.wpi.first.wpilibj.templates.subsystems;
 
+import edu.wpi.first.wpilibj.AnalogTrigger;
 import edu.wpi.first.wpilibj.templates.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -15,6 +16,22 @@ public class Switches extends Subsystem {
 
     public Switches() {
     }
+    
+    public void initSwitches(){
+        this.balanceArmSwitches.setLimitsVoltage(1, 1);
+        this.loaderFrontSwitch.setLimitsVoltage(1, 1);
+        this.elevatorBottomSwitch.setLimitsVoltage(1, 1);
+        this.elevatorTopSwitch.setLimitsVoltage(1, 1);
+    }
+    
+    private AnalogTrigger balanceArmSwitches = new AnalogTrigger(RobotMap.BALANCE_ARM_SWITCHES);
+    
+    // The switch at the front of the loader
+    private AnalogTrigger loaderFrontSwitch = new AnalogTrigger(RobotMap.LOADER_FRONT_SWITCH);
+    // The switch at the bottom of the elevator
+    private AnalogTrigger elevatorBottomSwitch = new AnalogTrigger(RobotMap.ELEVATOR_BOTTOM_SWITCH);
+    // The switch at the top of the elevator
+    private AnalogTrigger elevatorTopSwitch = new AnalogTrigger(RobotMap.ELEVATOR_TOP_SWITCH);
 
     public void readSwtiches() {
 
@@ -23,7 +40,11 @@ public class Switches extends Subsystem {
         SmartDashboard.putBoolean("Joystick Drive Enabled", CommandBase.globalState.isJoystickDriveEnabled());
 
         // Read balanceArm limit switches
+        CommandBase.globalState.setArmSwitch(balanceArmSwitches.getTriggerState());
 
         // Read elevator ball switches
+        CommandBase.globalState.setLoaderFrontSwitch(loaderFrontSwitch.getTriggerState());
+        CommandBase.globalState.setElevatorBottomSwitch(elevatorBottomSwitch.getTriggerState());
+        CommandBase.globalState.setElevatorTopSwitch(elevatorTopSwitch.getTriggerState());
     }
 }
