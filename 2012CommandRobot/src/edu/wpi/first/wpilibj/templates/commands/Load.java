5f49515f1bfda7502c6d1ec567ebe5ad659e868e
Ingyram <Ingyram@10.94.250.27>
2012-02-11 17:51:09 -0800

    Modified GlobalState

--- 2012CommandRobot/src/edu/wpi/first/wpilibj/templates/commands/Load.java
+++ 2012CommandRobot/src/edu/wpi/first/wpilibj/templates/commands/Load.java
@@ -16,18 +16,18 @@ public class Load extends CommandBase {
     protected void execute() {
 
         // Raise the elevator if and only if we have balls in control and no ball ready to shoot
-        if (!CommandBase.globalState.readyToShoot() && CommandBase.globalState.ballsInControl() > 0 && CommandBase.globalState.getBallsAboveBottomOfElevator()>0) {
-            CommandBase.elevator.run(RobotMap.ELEVATOR_SPEED_LOAD);
+        if (!globalState.readyToShoot() && globalState.ballsInControl() > 0 && globalState.getBallsAboveBottomOfElevator()>0) {
+            elevator.run(RobotMap.ELEVATOR_SPEED_LOAD);
         } else {
             elevator.stop();
         }
-        CommandBase.loader.run(RobotMap.LOADER_SPEED);
+        loader.run(RobotMap.LOADER_SPEED);
     }
 
     // Done loading when there are no balls in the loader, no balls in at the bottom of the elevator
     // OR readyToShoot() returns true.
     protected boolean isFinished() {
-        return (CommandBase.globalState.getBallsInLoader()==0)&&(CommandBase.globalState.getBallsAboveBottomOfElevator()==0)&&(this.isTimedOut())||CommandBase.globalState.readyToShoot();
+        return (globalState.getBallsInLoader()==0)&&(globalState.getBallsAboveBottomOfElevator()==0)&&(this.isTimedOut())||globalState.readyToShoot();
     }
 
     protected void end() {
