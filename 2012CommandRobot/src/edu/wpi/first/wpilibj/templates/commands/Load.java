256d59b15674791e6c5cd02bef92ab1a22ecdee0
Zachary Kotlarek <zach@kotlarek.com>
2012-02-14 03:33:11 -0800

    Run the load wheels continously while in load mode. Self-cancel if a ball reaches the top of the elevator or when we have 3 balls in control and none in the loader wheels.

--- 2012CommandRobot/src/edu/wpi/first/wpilibj/templates/commands/Load.java
+++ 2012CommandRobot/src/edu/wpi/first/wpilibj/templates/commands/Load.java
@@ -10,24 +10,34 @@ public class Load extends CommandBase {
     }
 
     protected void initialize() {
-        this.setTimeout(2);
     }
 
     protected void execute() {
 
-        // Raise the elevator if and only if we have balls in control and no ball ready to shoot
-        if (!CommandBase.globalState.readyToShoot() && CommandBase.globalState.ballsInControl() > 0 && CommandBase.globalState.getBallsAboveBottomOfElevator()>0) {
-            CommandBase.elevator.run(RobotMap.ELEVATOR_SPEED_LOAD);
+        /*
+         * Raise the elevator if any only if:
+         *
+         * We have a ball between the bottom and middle elevator switches
+         * We do not have a ball at the top of the elevator
+         */
+        if (!globalState.readyToShoot() && globalState.getBallsAboveBottomOfElevator() > 0) {
+            elevator.run(RobotMap.ELEVATOR_SPEED_LOAD);
         } else {
             elevator.stop();
         }
-        CommandBase.loader.run(RobotMap.LOADER_SPEED);
+        loader.run(RobotMap.LOADER_SPEED);
     }
 
-    // Done loading when there are no balls in the loader, no balls in at the bottom of the elevator
-    // OR readyToShoot() returns true.
+    /*
+     * Stop loading if we have a ball at the top of the elevator or we cannot legally pick up more balls
+     */
     protected boolean isFinished() {
-        return (CommandBase.globalState.getBallsInLoader()==0)&&(CommandBase.globalState.getBallsAboveBottomOfElevator()==0)&&(this.isTimedOut())||CommandBase.globalState.readyToShoot();
+        if (globalState.readyToShoot()) {
+            return true;
+        } else if (globalState.ballsInControl() >= RobotMap.MAX_BALLS && globalState.getBallsInLoader() < 1) {
+            return true;
+        }
+        return false;
     }
 
     protected void end() {
