45bf1f1f514a2bf833e5a24512581b58f8bac45f
Zachary Kotlarek <zach@kotlarek.com>
2012-02-16 01:37:32 -0800

    Use the existing shoot speed def

--- 2012CommandRobot/src/edu/wpi/first/wpilibj/templates/commands/SpinShooter.java
+++ 2012CommandRobot/src/edu/wpi/first/wpilibj/templates/commands/SpinShooter.java
@@ -10,48 +10,35 @@ import edu.wpi.first.wpilibj.templates.RobotMap;
  *
  * @author prog
  */
-public class SpinShooter extends CommandBase
-{
+public class SpinShooter extends CommandBase {
+
     private double speed;
-    
-    public SpinShooter(double speed){
+
+    public SpinShooter(double speed) {
         requires(shooter);
-        this.speed=speed;
-    }
-    public SpinShooter()
-    {
-        this(RobotMap.DEFAULT_MOTOR_SPEED);
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+        this.speed = speed;
     }
 
-    // Called just before this Command runs the first time
-    protected void initialize()
-    {
+    public SpinShooter() {
+        this(RobotMap.SHOOTER_SPEED);
     }
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute()
-    {
+    protected void initialize() {
+    }
+
+    protected void execute() {
         shooter.setSetpoint(speed);
     }
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished()
-    {
+    protected boolean isFinished() {
         return false;
     }
 
-    // Called once after isFinished returns true
-    protected void end()
-    {
+    protected void end() {
         shooter.setSetpoint(0);
     }
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted()
-    {
-        end();
+    protected void interrupted() {
+        this.end();
     }
 }
