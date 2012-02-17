0353f13f82735ca77e2272e2ad6a99ff6493348b
Zachary Kotlarek <zach@kotlarek.com>
2012-02-16 20:30:30 -0800

    Format

--- 2012CommandRobot/src/edu/wpi/first/wpilibj/templates/subsystems/ShooterEncoder.java
+++ 2012CommandRobot/src/edu/wpi/first/wpilibj/templates/subsystems/ShooterEncoder.java
@@ -1,35 +1,23 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.wpi.first.wpilibj.templates.subsystems;
 
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.templates.RobotMap;
-import edu.wpi.first.wpilibj.templates.commands.ReadGravity;
 import edu.wpi.first.wpilibj.templates.commands.ReadShooterEncoder;
 
-/**
- *
- * @author prog
- */
-public class ShooterEncoder extends Subsystem
-{
-    private Encoder encoder;
-    public ShooterEncoder(){
-        encoder=new Encoder(RobotMap.ENCODER_CHANNEL_A,RobotMap.ENCODER_CHANNEL_B);
-    }
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+public class ShooterEncoder extends Subsystem {
 
-    public void initDefaultCommand()
-    {  
+    private Encoder encoder;
+
+    public ShooterEncoder() {
+        encoder = new Encoder(RobotMap.ENCODER_CHANNEL_A, RobotMap.ENCODER_CHANNEL_B);
+    }
+
+    public void initDefaultCommand() {
         setDefaultCommand(new ReadShooterEncoder());
     }
-    public double getRate(){
+
+    public double getRate() {
         return encoder.getRate();
     }
-    
-    
 }
