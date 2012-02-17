101c4510f8f36b1925891272b077d46ca6ef3a1c
Zachary Kotlarek <zach@kotlarek.com>
2012-02-16 20:26:09 -0800

    Format

--- 2012CommandRobot/src/edu/wpi/first/wpilibj/templates/commands/ReadHoodAngle.java
+++ 2012CommandRobot/src/edu/wpi/first/wpilibj/templates/commands/ReadHoodAngle.java
@@ -1,48 +1,27 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.wpi.first.wpilibj.templates.commands;
 
-/**
- *
- * @author prog
- */
-public class ReadHoodAngle extends CommandBase
-{
-    
-    public ReadHoodAngle()
-    {
+
+public class ReadHoodAngle extends CommandBase {
+
+    public ReadHoodAngle() {
         requires(hoodAngle);
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
     }
 
-    // Called just before this Command runs the first time
-    protected void initialize()
-    {
+    protected void initialize() {
     }
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute()
-    {
+    protected void execute() {
         globalState.setHoodAngle(hoodAngle.getPosition());
     }
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished()
-    {
+    protected boolean isFinished() {
         return false;
     }
 
-    // Called once after isFinished returns true
-    protected void end()
-    {
+    protected void end() {
     }
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted()
-    {
+    protected void interrupted() {
+        this.end();
     }
 }
