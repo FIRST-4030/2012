04ea394a65bbf5224d953a94e140edf1f812503d
Zachary Kotlarek <zach@kotlarek.com>
2012-02-18 18:50:00 -0800

    It's a Victor

--- 2012CommandRobot/src/edu/wpi/first/wpilibj/templates/subsystems/Loader.java
+++ 2012CommandRobot/src/edu/wpi/first/wpilibj/templates/subsystems/Loader.java
@@ -2,20 +2,20 @@ package edu.wpi.first.wpilibj.templates.subsystems;
 
 import edu.wpi.first.wpilibj.templates.RobotMap;
 
-import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Loader extends Subsystem {
 
-    Jaguar loader;
+    Victor loader;
 
     protected void initDefaultCommand() {
         setDefaultCommand(null);
     }
 
     public Loader() {
-        loader = new Jaguar(RobotMap.MOTOR_LOADER);
+        loader = new Victor(RobotMap.MOTOR_LOADER);
     }
 
     public void run(double speed) {
