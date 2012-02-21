2445bf4ca042c0962f8556b5685b8e770f127bb5
Zachary Kotlarek <zach@kotlarek.com>
2012-02-21 15:44:15 -0800

    Try the counter class

--- 2012CommandRobot/src/edu/wpi/first/wpilibj/templates/subsystems/ShooterEncoder.java
+++ 2012CommandRobot/src/edu/wpi/first/wpilibj/templates/subsystems/ShooterEncoder.java
@@ -1,7 +1,7 @@
 package edu.wpi.first.wpilibj.templates.subsystems;
 
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Counter;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.templates.RobotMap;
@@ -9,13 +9,11 @@ import edu.wpi.first.wpilibj.templates.commands.ReadShooterEncoder;
 
 public class ShooterEncoder extends Subsystem {
 
-    private Encoder encoder;
-    DigitalInput a=new DigitalInput(RobotMap.ENCODER_CHANNEL_A);
-    DigitalInput b=new DigitalInput(RobotMap.ENCODER_CHANNEL_B);
-
+    private Counter encoder;
+    DigitalInput a = new DigitalInput(RobotMap.ENCODER_CHANNEL_A);
+    
     public ShooterEncoder() {
-        encoder = new Encoder(a, b);
-        encoder.setDistancePerPulse(1);
+        encoder = new Counter(a);
     }
 
     public void initDefaultCommand() {
@@ -23,12 +21,8 @@ public class ShooterEncoder extends Subsystem {
     }
 
     public double getRate() {
-        double rate = encoder.getRate();
+        double rate = 1.0 / encoder.getPeriod();
         SmartDashboard.putDouble("Shooter Rate", rate);
-        SmartDashboard.putDouble("Shooter Raw", encoder.getRaw());
-        SmartDashboard.putBoolean("Channel A", a.get());
-        SmartDashboard.putBoolean("Channel B", b.get());
-        SmartDashboard.putDouble("Get_Period", encoder.getPeriod());
         return rate;
     }
 }
