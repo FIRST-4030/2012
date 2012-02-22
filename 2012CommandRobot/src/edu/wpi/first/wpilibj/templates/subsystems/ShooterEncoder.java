21d5b7bc4792b8f3212dc2545eee048fd5aa2dd9
Zachary Kotlarek <zach@kotlarek.com>
2012-02-21 22:04:10 -0800

    This is so not cool

--- 2012CommandRobot/src/edu/wpi/first/wpilibj/templates/subsystems/ShooterEncoder.java
+++ 2012CommandRobot/src/edu/wpi/first/wpilibj/templates/subsystems/ShooterEncoder.java
@@ -2,6 +2,7 @@ package edu.wpi.first.wpilibj.templates.subsystems;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Counter;
+import edu.wpi.first.wpilibj.Utility;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.templates.RobotMap;
@@ -9,9 +10,11 @@ import edu.wpi.first.wpilibj.templates.commands.ReadShooterEncoder;
 
 public class ShooterEncoder extends Subsystem {
 
+    private double lastTime = 0;
+    private double rate = 0.0;
     private Counter encoder;
     DigitalInput a = new DigitalInput(RobotMap.ENCODER_CHANNEL_A);
-    
+
     public ShooterEncoder() {
         encoder = new Counter(a);
         encoder.start();
@@ -22,8 +25,18 @@ public class ShooterEncoder extends Subsystem {
     }
 
     public double getRate() {
-        double rate = 1.0 / encoder.getPeriod();
+        double time = (Utility.getFPGATime() / 1000000);
+        if (time > lastTime + RobotMap.ENCODER_UPDATE_PERIOD) {
+            rate = (double) encoder.get() / (time - lastTime);
+            SmartDashboard.putDouble("Shooter Count", (double) encoder.get());
+            encoder.reset();
+            lastTime = time;
+            SmartDashboard.putDouble("Shooter Time", time);
+            SmartDashboard.putDouble("Shooter Last Time", lastTime);
+            SmartDashboard.putDouble("Shooter Time Diff", time - lastTime);
+        }
         SmartDashboard.putDouble("Shooter Rate", rate);
-        return rate;
+        SmartDashboard.putBoolean("Shooter A", a.get());
+        return 0;
     }
 }
