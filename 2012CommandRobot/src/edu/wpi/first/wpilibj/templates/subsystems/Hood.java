9e9e7a420d338b99cd2b1d746a92d63501a2c5f3
Zachary Kotlarek <zach@kotlarek.com>
2012-02-22 01:43:44 -0800

    Add a minimum threshold for the hood PID

--- 2012CommandRobot/src/edu/wpi/first/wpilibj/templates/subsystems/Hood.java
+++ 2012CommandRobot/src/edu/wpi/first/wpilibj/templates/subsystems/Hood.java
@@ -1,9 +1,8 @@
 package edu.wpi.first.wpilibj.templates.subsystems;
 
-import edu.wpi.first.wpilibj.Utility;
 import edu.wpi.first.wpilibj.templates.RobotMap;
 import edu.wpi.first.wpilibj.templates.commands.CommandBase;
-//import edu.wpi.first.wpilibj.templates.commands.MoveHood;
+import edu.wpi.first.wpilibj.templates.commands.MoveHood;
 
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
@@ -14,16 +13,16 @@ public class Hood extends PIDSubsystem {
     Victor hood;
 
     public Hood() {
-        super("HoodAngle", RobotMap.HOOD_P_GAIN, RobotMap.HOOD_I_GAIN, RobotMap.HOOD_D_GAIN, RobotMap.HOOD_PID_PERIOD);
-
+        super("HoodAngle", RobotMap.HOOD_P_GAIN, RobotMap.HOOD_I_GAIN,
+                RobotMap.HOOD_D_GAIN, RobotMap.HOOD_PID_PERIOD);
 
         this.setSetpointRange(RobotMap.HOOD_ANGLE_MIN, RobotMap.HOOD_ANGLE_MAX);
         this.setSetpoint((RobotMap.HOOD_ANGLE_MIN + RobotMap.HOOD_ANGLE_MAX) / 2);
-        this.getPIDController().setTolerance(RobotMap.HOOD_PID_TOLERANCE);
         hood = new Victor(RobotMap.MOTOR_HOOD);
     }
 
     public void initDefaultCommand() {
+        this.setDefaultCommand(new MoveHood());
     }
 
     protected double returnPIDInput() {
@@ -34,12 +33,22 @@ public class Hood extends PIDSubsystem {
         SmartDashboard.putDouble("Hood Calculated Speed", output);
         SmartDashboard.putDouble("Hood Setpoint", this.getSetpoint());
 
+        // Provide a minimum threshold under which we never move
+        // Also reset the PID controller to avoid accumulating error
+        if (Math.abs(CommandBase.globalState.getHoodAngle() - this.getSetpoint())
+                < RobotMap.HOOD_PID_TOLERANCE) {
+            output = 0;
+            this.getPIDController().reset();
+        }
+
         // Prevent hood overruns
         // The mistmatch bewteen MAX and command direction is intentional -- the hood runs backwards with respect to the pot
         if ((CommandBase.globalState.getHoodAngle() >= RobotMap.HOOD_ANGLE_MAX && output < 0)
                 || (CommandBase.globalState.getHoodAngle() <= RobotMap.HOOD_ANGLE_MIN && output > 0)) {
             output = 0;
         }
+
+
         hood.set(output);
         SmartDashboard.putDouble("Hood Command Speed", output);
     }
@@ -51,7 +60,6 @@ public class Hood extends PIDSubsystem {
         } else if (this.getSetpoint() < RobotMap.HOOD_ANGLE_MIN) {
             this.setSetpoint(RobotMap.HOOD_ANGLE_MIN);
         }
-        start();
     }
 
     public void start() {
